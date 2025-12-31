ad80c8846174b5b5f77c04674db55fff4133a3e9
Huy Tran <huytp@zigexn.vn>
2025-12-31 14:45:37 +0700

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::API
   # API only controller
+  before_action :authenticate_user
+
+  protected
+
+  def authenticate_user
+    token = request.headers['Authorization']&.split(' ')&.last
+    @current_user = User.find_by(token: token) if token
+
+    unless @current_user&.token_valid?
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+  end
 end
 
--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,4 +1,6 @@
 class HealthController < ApplicationController
+  skip_before_action :authenticate_user
+
   def check
     render json: {
       status: 'ok',
